d09695a47f7a9c28879f18b121e8de23b954c2a4
Remi Thebault <remi.thebault@gmail.com>
2022-04-21 21:04:09 +0200

    compressXz implementation

---
+++ src/squiz_box/xz.d
@@ -0,0 +1,92 @@
+module squiz_box.xz;
+
+import squiz_box.core;
+import squiz_box.c.lzma;
+
+import std.exception;
+
+auto compressXz(BR)(BR input, uint level=6, size_t chunkSize=4096)
+if (isByteRange!BR)
+{
+    return CompressXz!BR(input, level, chunkSize);
+}
+
+private struct CompressXz(BR)
+if (isByteRange!BR)
+{
+    import std.conv : to;
+
+    private BR _input;
+    private uint _level;
+    private lzma_stream _stream;
+
+    private ubyte[] _inChunk;
+
+    private ubyte[] _outBuffer;
+    private ubyte[] _outChunk;
+
+    this (BR input, uint level, size_t chunkSize)
+    {
+        _input = input;
+        _level = level;
+
+        _outBuffer = new ubyte[chunkSize];
+
+        const ret = lzma_easy_encoder(&_stream, level, lzma_check.CRC64);
+        enforce(ret == lzma_ret.OK, "Could not initialize LZMA encoder: " ~ ret.to!string);
+
+        prime();
+    }
+
+    private void prime()
+    {
+
+        while(_outChunk.length < _outBuffer.length)
+        {
+            if (_inChunk.length == 0 && !_input.empty)
+                _inChunk = _input.front;
+
+            _stream.next_in = _inChunk.ptr;
+            _stream.avail_in = _inChunk.length;
+            _stream.total_in = 0;
+
+            _stream.next_out = _outBuffer.ptr + _outChunk.length;
+            _stream.avail_out = _outBuffer.length - _outChunk.length;
+            _stream.total_out = 0;
+
+            const action = _input.empty ? lzma_action.FINISH : lzma_action.RUN;
+            const res = lzma_code(&_stream, action);
+
+            _inChunk = _inChunk[_stream.total_in .. $];
+
+            const outEnd = _outChunk.length + _stream.total_out;
+            _outChunk = _outBuffer[0 .. outEnd];
+
+            // popFront must be called at the end because it may invalidate
+            // inChunk with some ranges.
+            if (_inChunk.length == 0 && !_input.empty)
+                _input.popFront();
+
+            if (res == lzma_ret.STREAM_END)
+                break;
+
+            enforce(res == lzma_ret.OK, "LZMA encoding failed with code: " ~ res.to!string);
+        }
+    }
+
+    @property bool empty()
+    {
+        return _outChunk.length == 0;
+    }
+
+    @property ubyte[] front()
+    {
+        return _outChunk;
+    }
+
+    void popFront()
+    {
+        _outChunk = null;
+        prime();
+    }
+}
