dccadf926e67f0e10449d5299766094ca1563c80
Remi Thebault <remi.thebault@gmail.com>
2022-04-23 01:06:18 +0200

    compress/decompress Gz

--- src/squiz_box/gz.d
+++ src/squiz_box/gz.d
@@ -5,11 +5,103 @@ import squiz_box.core;
 import squiz_box.priv;
 
 import std.exception;
+import std.typecons;
 
-auto compressGz(BR)(BR input, uint level = 6, size_t chunkSize = 4096)
-        if (isByteRange!BR)
+auto compressGz(I)(I input, uint level = 6, size_t chunkSize = defaultChunkSize)
+        if (isByteRange!I)
 {
-    return CompressGz!BR(input, level, chunkSize);
+    CompressGzPolicy params;
+    params.level = level;
+    return CompressDecompressAlgo!(I, CompressGzPolicy)(input, chunkSize, params);
+}
+
+private struct CompressGzPolicy
+{
+    alias Stream = z_stream;
+
+    int level = 6;
+    int windowBits = 15;
+    int memLevel = 8;
+    int strategy = Z_DEFAULT_STRATEGY;
+
+    static Stream* initialize(CompressGzPolicy params)
+    {
+        auto stream = new z_stream;
+        stream.zalloc = &(gcAlloc!uint);
+        stream.zfree = &gcFree;
+
+        const ret = deflateInit2(
+            stream, params.level, Z_DEFLATED,
+            16 + params.windowBits /* +16 for gzip instead of zlib wrapper */ ,
+            params.memLevel,
+            params.strategy);
+
+        enforce(
+            ret == Z_OK,
+            "Could not initialize Zlib encoder: " ~ zResultToString(ret)
+        );
+
+
+        return stream;
+    }
+
+    static Flag!"streamEnded" process(Stream* stream, bool inputEmpty)
+    {
+        const flush = inputEmpty ? Z_FINISH : Z_NO_FLUSH;
+        const res = deflate(stream, flush);
+        if (res == Z_STREAM_END)
+            return Yes.streamEnded;
+        enforce(
+            res == Z_OK,
+            "Zlib deflate failed with code: " ~ zResultToString(res)
+        );
+        return No.streamEnded;
+    }
+}
+
+auto decompressGz(I)(I input, size_t chunkSize = defaultChunkSize)
+if (isByteRange!I)
+{
+    DecompressGzPolicy params;
+    return CompressDecompressAlgo!(I, DecompressGzPolicy)(input, chunkSize, params);
+}
+
+private struct DecompressGzPolicy
+{
+    alias Stream = z_stream;
+
+    int windowBits = 15;
+
+    static Stream* initialize(DecompressGzPolicy params)
+    {
+        auto stream = new z_stream;
+        stream.zalloc = &(gcAlloc!uint);
+        stream.zfree = &gcFree;
+
+        const ret = inflateInit2(
+            stream, 16 + params.windowBits /* +16 for gzip instead of zlib wrapper */ ,
+        );
+
+        enforce(
+            ret == Z_OK,
+            "Could not initialize Zlib inflate stream: " ~ zResultToString(ret)
+        );
+
+        return stream;
+    }
+
+    static Flag!"streamEnded" process(Stream* stream, bool inputEmpty)
+    {
+        const flush = Z_NO_FLUSH;
+        const res = inflate(stream, flush);
+        if (res == Z_STREAM_END)
+            return Yes.streamEnded;
+        enforce(
+            res == Z_OK,
+            "Zlib inflate failed with code: " ~ zResultToString(res)
+        );
+        return No.streamEnded;
+    }
 }
 
 private string zResultToString(int res)
@@ -89,125 +181,4 @@ private void zPrintStream(z_stream* strm, string label)
     writeln("    total_out = ", strm.total_out);
     if (strm.msg)
         writeln("    msg = ", fromStringz(strm.msg));
-}
-
-private enum logGz = false;
-
-private struct CompressGz(BR) if (isByteRange!BR)
-{
-    import std.conv : to;
-
-    private BR _input;
-    private uint _level;
-    private z_stream* _stream;
-
-    private ubyte[] _inChunk;
-
-    private ubyte[] _outBuffer;
-    private ubyte[] _outChunk;
-
-    private bool _ended;
-
-    this(BR input, uint level, size_t chunkSize)
-    {
-        _input = input;
-        _level = level;
-
-        // allocating on the heap to ensure the address never changes
-        // which would creates stream error;
-        _stream = new z_stream;
-        _stream.zalloc = &(gcAlloc!uint);
-        _stream.zfree = &gcFree;
-
-        _outBuffer = new ubyte[chunkSize];
-
-        const ret = deflateInit2(
-            _stream, level, Z_DEFLATED,
-            16 + 15 /* +16 for gzip instead of zlib wrapper */ ,
-            8,
-            Z_DEFAULT_STRATEGY);
-
-        enforce(
-            ret == Z_OK,
-            "Could not initialize Zlib encoder: " ~ zResultToString(ret)
-        );
-
-        prime();
-    }
-
-    private void prime()
-    {
-        static if (logGz)
-        {
-            import std.stdio;
-        }
-
-        while (_outChunk.length < _outBuffer.length)
-        {
-            if (_inChunk.length == 0 && !_input.empty)
-                _inChunk = _input.front;
-
-            _stream.next_in = _inChunk.ptr;
-            _stream.avail_in = cast(uint) _inChunk.length;
-
-            _stream.next_out = _outBuffer.ptr + _outChunk.length;
-            _stream.avail_out = cast(uint)(_outBuffer.length - _outChunk.length);
-
-            const flush = _input.empty ? Z_FINISH : Z_NO_FLUSH;
-
-            static if (logGz)
-            {
-                zPrintStream(_stream, "before");
-                writeln("Flush = ", zFlushToString(flush));
-            }
-
-            const res = deflate(_stream, flush);
-
-            static if (logGz)
-            {
-                zPrintStream(_stream, "after");
-                writeln("Deflate result = ", zResultToString(res));
-                writeln();
-            }
-
-            const readIn = _inChunk.length - _stream.avail_in;
-            _inChunk = _inChunk[readIn .. $];
-
-            const outEnd = _outBuffer.length - _stream.avail_out;
-            _outChunk = _outBuffer[0 .. outEnd];
-
-            // popFront must be called at the end because it may invalidate
-            // inChunk with some ranges.
-            if (_inChunk.length == 0 && !_input.empty)
-                _input.popFront();
-
-            if (res == Z_STREAM_END)
-            {
-                _ended = true;
-                break;
-            }
-
-            enforce(
-                res == Z_OK,
-                "Zlib deflate failed with code: " ~ zResultToString(res)
-            );
-        }
-    }
-
-    @property bool empty()
-    {
-        return _outChunk.length == 0;
-    }
-
-    @property ubyte[] front()
-    {
-        return _outChunk;
-    }
-
-    void popFront()
-    {
-        _outChunk = null;
-        if (!_ended)
-            prime();
-    }
 }
