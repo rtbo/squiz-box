6d06fdfdced6dd5597150f6741ba8c882502b12f
Remi Thebault <remi.thebault@gmail.com>
2023-01-09 01:07:18 +0100

    DataIntegrity7zArchiveException

--- src/squiz_box/box/seven_z/error.d
+++ src/squiz_box/box/seven_z/error.d
@@ -47,6 +47,23 @@ class Unsupported7zArchiveException : SevenZArchiveException
     }
 }
 
+class DataIntegrity7zArchiveException : SevenZArchiveException
+{
+    string archive;
+    string entry;
+    uint expectedCrc;
+    uint currentCrc;
+
+    this(string archive, string entry, uint expectedCrc, uint currentCrc)
+    {
+        this.archive = archive;
+        this.entry = entry;
+        this.expectedCrc = expectedCrc;
+        this.currentCrc = currentCrc;
+        super(format!"Data integrity error for entry '%s' in archive '%s'"(entry, archive));
+    }
+}
+
 package(squiz_box.box.seven_z):
 
 noreturn bad7z(string source, string reason, string file = __FILE__, size_t line = __LINE__)
